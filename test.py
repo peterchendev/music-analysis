4350f1779395a4964256d25675636d441410fbfc
Peter Chen <peterchen@Peters-MacBook-Air-501.local>
2026-05-29 23:38:55 -0500

    Initial music analysis notebook

---
+++ test.py
@@ -0,0 +1,13 @@
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+
+
+#Check versions
+
+print("librosa:", librosa.__version__)
+print("numpy:", np.__version__)
+print("matplotlib:", plt.matplotlib.__version__)
+print("scipy:", scipy.__version__)
+print("All good!")
